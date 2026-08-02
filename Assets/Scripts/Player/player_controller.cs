ec221883c2fbcdd7ab8ac3be5a93f0d639b7d675
duskarr <realizanhh15@gmail.com>
2026-08-02 16:13:36 +0800

    sprite animations

--- Assets/Scripts/Player/player_controller.cs
+++ Assets/Scripts/Player/player_controller.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     public float speed = 5f;
 
     void Update()
@@ -15,5 +17,23 @@ public class PlayerMovement : MonoBehaviour
         if (Keyboard.current.dKey.isPressed) input.x += 1;
 
         transform.position += (Vector3)input.normalized * speed * Time.deltaTime;
+
+        if (input != Vector2.zero)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (input.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (input.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
